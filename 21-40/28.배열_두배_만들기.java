0bd57a39c4dbfdd6aeb620c2e69211930bff2ef4
choibonggeun <dhy00085@naver.com>
2023-04-01 20:35:00 +0900

    28-29

---
+++ 21-40/28.배열_두배_만들기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        for(int i = 0; i < numbers.length; i++)
+        {
+            numbers[i] += numbers[i];
+        }
+        return numbers;
+    }
+}
+
+/*
+ import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
+    }
+}
+
+ */
