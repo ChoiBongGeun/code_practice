3b64680bca05822d0696437cd069df86256fd1d8
ChoiBongGeun <dhy00085@naver.com>
2023-07-04 16:24:48 +0900

    정리

--- 이분탐색법/입국심사.java
+++ 이분탐색법/입국심사.java
@@ -4,16 +4,21 @@ class Solution {
         long answer = 0;
         Arrays.sort(times);
         long min = 0;
-        long max = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음
+        long max = times[times.length-1] * (long)n; //모든 사람이 가장 느리게 심사받음  
         
         while(min <= max) {
             long mid = (min + max) / 2;
             long complete = 0;
             for (int i = 0; i < times.length; i++)
+            {
                 complete += mid / times[i];
+            }
             if (complete < n) // 해당 시간에는 모든 사람이 검사받을 수 없다.
+            {
                 min = mid + 1;
-            else {
+            }
+            else 
+            {
                 max = mid - 1;
                 answer = mid; // 모두 검사받았으나, 더 최솟값이 있을 수 있다.
             }
@@ -21,3 +26,8 @@ class Solution {
         return answer;
     }
 }
+/*
+ max 60         29      29     29    29
+ min 0           0      15     23    27
+ complete 7      3      5       5    6
+ */
